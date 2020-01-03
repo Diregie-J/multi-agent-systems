938c78bf32e95b4def680dc1f2a73db24350afc5
Vomyrak <vomyrak@hotmail.com>
2020-01-03 11:47:05 +0000

    Agent are now only sanctioned on the day of crime detection

--- multi-agent-systems/Sanctions.fs
+++ multi-agent-systems/Sanctions.fs
@@ -78,18 +78,21 @@ let infamyDecay (world: WorldState) (agents: Agent list) : Agent list =
 let sanction (world: WorldState) (agents: Agent list) : Agent list = 
     agents
     |> List.map (fun el ->
-        if el.Infamy <= 0.2 then {el with AccessToShelter = el.AccessToShelter;
-                                            AccessToFood = true}
-        elif el.Infamy <= 0.5 then {el with AccessToShelter = None;
-                                            AccessToFood = true}
-        elif el.Infamy <= 0.9 then {el with AccessToFood = false;
-                                            AccessToShelter = None}
-        else 
-            match world.CurrentMaxPunishment with 
-            | NoFoodAndShelter -> {el with AccessToFood = false;
-                                            AccessToShelter = None}
-            | Exile -> {el with Alive = false}
-            | _ -> failwith "Invalid maximum punishment setting"
+        if el.LastCrimeDate = world.CurrentDay then
+            if el.Infamy <= 0.2 then {el with AccessToShelter = el.AccessToShelter;
+                                                AccessToFood = true}
+            elif el.Infamy <= 0.5 then {el with AccessToShelter = None;
+                                                AccessToFood = true}
+            elif el.Infamy <= 0.9 then {el with AccessToFood = false;
+                                                AccessToShelter = None}
+            else 
+                match world.CurrentMaxPunishment with 
+                | NoFoodAndShelter -> {el with AccessToFood = false;
+                                                AccessToShelter = None}
+                | Exile -> {el with Alive = false}
+                | _ -> failwith "Invalid maximum punishment setting"
+        else {el with AccessToShelter = el.AccessToShelter;
+                        AccessToFood = true}
     )
 
 
